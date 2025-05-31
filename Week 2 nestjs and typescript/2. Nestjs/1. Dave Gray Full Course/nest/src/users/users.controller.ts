e1fbd775d192331d77d944a11523eef7f12dda11
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-31 19:16:09 +0530

    Controllers

---
+++ Week 2 nestjs and typescript/2. Nestjs/1. Dave Gray Full Course/nest/src/users/users.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Get, Param, Patch, Post, Delete, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    /*
+        GET /users
+        GET /users/:id
+        POST /users
+        PATCH /users/:id
+        DELETE /users/:id
+
+        */
+
+        //query parameters
+    @Get()  // GET /users OR /users?role=value&age=42
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return []
+    }
+
+    // @Get('interns') // GET /users/interns
+    // findAllInterns() {
+    //     return []
+    // }
+
+    @Get(':id')  // GET /users:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post()  //POST /users
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id')  // PATCH /users:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+
+    @Delete(':id')  // DELETE /users:id
+    delete(@Param('id') id: string) {
+        return { id }
+    }
+
+
+}
