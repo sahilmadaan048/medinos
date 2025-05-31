345bec8eab1322945ce5a577ad33d0d9a73236cb
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-31 19:37:59 +0530

    Providers and Dependency injection

--- Week 2 nestjs and typescript/2. Nestjs/1. Dave Gray Full Course/nest/src/users/users.controller.ts
+++ Week 2 nestjs and typescript/2. Nestjs/1. Dave Gray Full Course/nest/src/users/users.controller.ts
@@ -1,46 +1,34 @@
-import { Body, Controller, Get, Param, Patch, Post, Delete, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    /*
-        GET /users
-        GET /users/:id
-        POST /users
-        PATCH /users/:id
-        DELETE /users/:id
 
-        */
+    constructor(private readonly usersService: UsersService) { }
 
-        //query parameters
-    @Get()  // GET /users OR /users?role=value&age=42
+    @Get() // GET /users or /users?role=value
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return []
+        return this.usersService.findAll(role)
     }
 
-    // @Get('interns') // GET /users/interns
-    // findAllInterns() {
-    //     return []
-    // }
-
-    @Get(':id')  // GET /users:id
+    @Get(':id') // GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id }
+        return this.usersService.findOne(+id)
     }
 
-    @Post()  //POST /users
-    create(@Body() user: {}) {
-        return user
+    @Post() // POST /users 
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.create(user)
     }
 
-    @Patch(':id')  // PATCH /users:id
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate }
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.update(+id, userUpdate)
     }
 
-    @Delete(':id')  // DELETE /users:id
+    @Delete(':id') // DELETE /users/:id
     delete(@Param('id') id: string) {
-        return { id }
+        return this.usersService.delete(+id)
     }
-
 
 }
