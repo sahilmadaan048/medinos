c63fc8c3b723ded6a42d79597ac7f284ec756538
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-06 12:41:01 +0530

    wrong correct it

---
+++ week 3 postgresql and prisma integration/1. Nestjs + prisma/nestjs-prisma/src/products/products.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly databaseSerice: DatabaseService) {}
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
